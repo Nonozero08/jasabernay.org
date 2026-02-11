bf9f78ec3935728ad8a625f40fab67a5bda7e108
Vince Linise <contact@vincelinise.com>
2026-02-11 20:17:26 +0100

    chore: New video steps

--- src/components/home/steps/steps.tsx
+++ src/components/home/steps/steps.tsx
@@ -8,59 +8,43 @@ export default function Steps() {
   return (
     <div id="connexion" className="section steps width">
       <div className="section-heading">
-        <h2 className="section-title">Conçue avec soin et attention au détail</h2>
+        <h2 className="section-title">Conçu avec soin et attention au détail</h2>
         <p className="section-description">
-          Papillon n'est pas juste un client de vie scolaire comme les autres. C'est une application pensée de fond en combre pour rendre l'école agréable et fluide.
+          Papillon est né de la volonté de donner une expérience utilisateur digne de ce nom aux services scolaires que l’on connaît, et cela passe par nos fonctionnalités.
         </p>
       </div>
 
       <div className="steps-list">
         <div className="step">
-          <img
-            src="/steps/step1.png"
-            alt="Step 1"
-            width={400}
-            height={200}
-            className="step-image"
-          />
           <h3 className="step-title">
-            L'essentiel au même endroit
+            À jour, toujours
           </h3>
           <p className="step-description">
-            Chaque composant est pensé pour apporter l'information essentielle au bon moment. Tout apparaît lorsque vous en avez besoin.
+            Papillon se synchronise avec vos services scolaires comme Pronote ou ÉcoleDirecte et affiche les informations pertinentes en temps réel au cours de la journée.
           </p>
+          <video src="/steps/step_updates.webm" autoPlay muted loop />
         </div>
 
         <div className="step">
-          <img
-            src="/steps/step2.png"
-            alt="Step 1"
-            width={400}
-            height={200}
-            className="step-image"
-          />
           <h3 className="step-title">
             Organisé pour vous
           </h3>
           <p className="step-description">
-            Vous n'avez plus besoin de penser à où chercher. Tout est centralisé, organisé et accessible en un seul endroit.
+            Grâce aux fonctionnalités d’organisation et d’interaction avec l’établissement, ne manquez plus un seul devoir. Tout est fluide, clair et facile à accomplir.
           </p>
+          <video src="/steps/step_task.webm" autoPlay muted loop />
+
         </div>
 
         <div className="step">
-          <img
-            src="/steps/step3.png"
-            alt="Step 1"
-            width={400}
-            height={200}
-            className="step-image"
-          />
           <h3 className="step-title">
-            Intégré à votre écosystème
+            Super-intelligent
           </h3>
           <p className="step-description">
-            L'interface Papillon est développée autour de Liquid Glass et des composants natifs pour s'intégrer parfaitement à votre environnement.
+            Ne perdez plus de temps à tout calculer. L’interface Papillon affiche vos résultats clairement, et vous permet même de naviguer dans votre moyenne générale.
           </p>
+          <video src="/steps/step_graph2.webm" autoPlay muted loop />
+
         </div>
       </div>
     </div>
